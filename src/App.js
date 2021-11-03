11b965af4e2fe0b3548e389f8379fc8aa316b91c
B H An <anbyunghoon0107@gamil.com>
2021-11-03 17:51:07 +0900

    useReducer, useMemo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Info from "./Info";
+import React, { useState } from "react";
+import Average from "./Average";
 
 const App = () => {
-  return <Info />;
+  return <Average />
 };
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
 
+    /* -------------------------------- useEffect ------------------------------- */
+    /* useEffect(() => {
+        console.log('rendering is complete!');
+        console.log({name, nickname});
+    }); */
+
+    /* ---------------------- useEffect (마운트될 때만 실행하고 싶을 때) --------------------- */
+    /* useEffect(() => {
+        console.log('마운트될 때만 실행됩니다.'); 
+    }, []); 마운트 될 때만 실행하고 싶을 시 뒤에 []를 붙여준다. */
+
+    useEffect(() => {
+        console.log('effect');
+        console.log(name);
+        return() => {
+            console.log('cleanup');
+            console.log(name);
+        }
+    }, [name]);
     const onChangeName = e => {
         setName(e.target.value);
     };
